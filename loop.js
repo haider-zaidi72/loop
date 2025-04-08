3a20a22fb7b2e727541e40a4c9fea3e862ada76f
urooj haider <suhz1100@gmail.com>
2025-04-08 14:59:21 +0500

    add

---
+++ loop.js
@@ -0,0 +1,20 @@
+// for (Intiate,  condition, increament);
+
+for (let i = 0; i < 2; i++) {
+    console.log (i);
+};
+
+
+let i=1;
+for (console.log("haider"); i<5; console.log("price incrase"),  ++i){
+    console.log(i);
+    
+};
+
+for (let i = 1 ; i < 10; i++){
+    console.log(i);
+}
+
+for (let i = 10 ; i >= 1; i--){
+    console.log(i);
+}
